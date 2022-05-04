520c01e289916e0158e0f847ee0c8b16b0541bc9
DeNic0la <nicola@fioretti.info>
2022-05-04 08:40:13 +0200

    added NgPrime and Cookie consent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ConfirmationService} from "primeng/api";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private confirmationService: ConfirmationService) {}
+  confirm() {
+    this.confirmationService.confirm({
+      message: 'Diese Webseite verwendet Kekse, Welche Kekse möchten sie ?',
+      accept: () => {
+        alert("Cokies angenommen")
+      },
+      reject: () =>{
+        alert("Cokies abgelehnt")
+      }
+    });
+  }
+
   title = 'M152-Page';
+
+  ngOnInit(): void {
+    this.confirm();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
+import {ConfirmationService} from 'primeng/api';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ConfirmDialogModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [
+    ConfirmationService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
