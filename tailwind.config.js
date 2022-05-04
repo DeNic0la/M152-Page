15c6134a42d38eff779616659ee4ce1d3d6f86ad
DeNic0la <nicola@fioretti.info>
2022-05-04 09:26:02 +0200

    configured tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      'primary': '#355C7D',
+      'accent': '#6C5B7B',
+      'secondary': '#C06C84',
+      'le-text': '#383838',
+      'success': '#8FB9A8',
+      'warn': '#FF964F',
+      'error': '#B80F0A',
+      'info': '#78A2CC',
+    },
   },
   plugins: [],
 }
