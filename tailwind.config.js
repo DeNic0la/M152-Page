f64c53c6000e34558ec72905377115e63cde50c8
DeNic0la <nicola@fioretti.info>
2022-06-11 15:48:10 +0200

    fixing colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,26 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
       "./src/**/*.{html,js}",
       "./pages/**/*.{html,js}"
   ],
   theme: {
-    extend: {},
-    colors: {
-      'primary': '#355C7D',
-      'accent': '#6C5B7B',
-      'secondary': '#C06C84',
-      'le-text': '#383838',
-      'success': '#8FB9A8',
-      'warn': '#FF964F',
-      'error': '#B80F0A',
-      'info': '#78A2CC',
-      'grey': '#808080',
-      'light-grey': '#909090',
-      'white-grey': '#d1d1d1',
-      'white':'#FFFFFF'
+    extend: {
+      colors: {
+        'primary': '#355C7D',
+        'accent': '#6C5B7B',
+        'secondary': '#C06C84',
+        'le-text': '#383838',
+        'success': '#8FB9A8',
+        'warn': '#FF964F',
+        'error': '#B80F0A',
+        'info': '#78A2CC',
+        'grey': '#808080',
+        'light-grey': '#909090',
+        'white-grey': '#d1d1d1',
+        'white':'#FFFFFF',
+      },
     },
     fontFamily: {
       'header': ['Black Ops One','system-ui'],
