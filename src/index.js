2b0aed4d9968ad3081260cf68880d38997f8765f
DeNic0la <nicola@fioretti.info>
2022-06-11 14:17:23 +0200

    added canvas

--- src/index.js
+++ src/index.js
@@ -3,3 +3,21 @@
 window.addEventListener('scroll', () => {
     document.body.style.setProperty('--scroll', window.scrollY / (document.body.offsetHeight - window.innerHeight));
 }, false);
+
+let c = document.getElementById("myCanvas");
+let ctx = c.getContext("2d");
+
+
+let count = 0;
+
+setInterval(textIncrement,1000);
+
+function textIncrement(){
+    ctx.fillStyle = '#ffffff';
+    ctx.fillText(count+"",count,count)
+
+    count++;
+
+    ctx.fillStyle = '#000000';
+    ctx.fillText(count+"",count,count)
+}
